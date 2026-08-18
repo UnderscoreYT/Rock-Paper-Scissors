f5e1189eb802db460c2f6e5c1cad411094f5fb1d
Darileo <Darileo@LAPTOP-4TD2BAEO>
2026-08-18 15:28:59 +0800

    Added a library of random choices for text messages.

--- 2) Rock-Paper-Scissors/2) Rock-Paper-Scissors.cpp
+++ 2) Rock-Paper-Scissors/2) Rock-Paper-Scissors.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 #include <algorithm>
+#include <array>
 #include <cctype>
 #include <random>
+#include <string_view>
 #include <unordered_map>
 using std::cout, std::cin, std::string;
 
-// GLOBALS
+
+
+
+// GLOBALS //
+class custom_messsages {
+
+public:
+
+    inline static constexpr std::array<std::string_view, 4> input_messages = {
+        "It's time for your choice! Rock, Paper, or Scissors?",
+        "No time to waste! Rock, Paper, or Scissors?!",
+        "This is not a waiting game, quick! Rock, Paper, or Scissors?",
+        "Time to gamble! Rock, Paper, or Scissors?"
+
+    };
+
+    inline static constexpr std::array<std::string_view, 4> loser_messages = {
+        "Stupid ass, follow the inputs! Try again.",
+		"You can't even follow simple instructions? Try again.",
+        "A toddler can figure it the third time. What are you, a fetus?",
+        "Ragebait or low IQ? You call it."
+    };
+
+}
 std::vector<string> game_elements = {"ROCK", "PAPER", "SCISSORS"};
 std::unordered_map<string, string> win_conditions = { // Ex. You can only win as rock if opponent is scissors.
     {"ROCK", "SCISSORS"},
@@ -17,6 +42,7 @@ bool game_ongoing = true;
 const int winningpoint_condition = 3; // Point system
 int player_points = 0; 
 int opponent_points = 0; 
+
 
 
 string random_choice() {
@@ -32,6 +58,8 @@ string random_choice() {
 
 int game_loop() 
 {
+    // Optimize the random choice to be more broad for other RNGs.
+
     cout << "\nIt's time for your choice! Rock, Paper, or Scissors?\n"
          << "(INPUT: ROCK, PAPER, SCISSORS)\n";
 
@@ -39,7 +67,7 @@ int game_loop()
     std::transform(user_input.begin(), user_input.end(), user_input.begin(), ::toupper); // Convert to Uppercase
 
     if (!(std::find(game_elements.begin(), game_elements.end(), user_input) != game_elements.end())) {
-        cout << "Stupid ass, follow the inputs! Try again.\n\n"; // Funny idea, make this more unhinged with more errors.
+        cout << "Stupid ass, follow the inputs! Try again.\n\n"; // (TODO: Funny idea, make this more unhinged with more errors.)
         return game_loop();
     }
 
@@ -80,8 +108,6 @@ int game_loop()
         game_ongoing = false;
         return 0;
     }
-
-    // In here, print the current point standing, and also check if one of them have won. (TODO)
 
     return 0;
 }
